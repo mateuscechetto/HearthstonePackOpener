94369eeedd7e958e0c53b90d3b9383339c883bac
mateus cechetto <mateuscechetto@gmail.com>
2021-04-07 19:58:31 -0300

    start function and open more packs question

--- openPacks.py
+++ openPacks.py
@@ -2,22 +2,38 @@ from pynput.keyboard import Key, Controller
 import time
 
 
-keyboard = Controller()
+def start():
+    keyboard = Controller()
 
-nPacks = int(input('number of packs: '))
+    nPacks = int(input("number of packs: "))
 
-print("tab back to hearthstone")
+    print("tab back to hearthstone")
 
-time.sleep(2)
+    time.sleep(2)
 
-t_end = time.time() + nPacks * 6
+    t_end = time.time() + nPacks * 6
 
 
-print("opening packs...")
+    print("opening packs...")
 
-while time.time() < t_end:
-    time.sleep(0.2)
-    keyboard.press(' ')
-    keyboard.release(' ')
+    while time.time() < t_end:
+        time.sleep(0.2)
+        keyboard.press(' ')
+        keyboard.release(' ')
 
-print(nPacks," packs opened")
+    print(nPacks," packs opened")
+    while True:
+        query = input("do you want to open more packs?").lower().strip()
+        if query == '' or  not query[0] in ['y','n']:
+            print("please answer with yes or no")
+        else:
+            break
+    
+    if query[0] == 'y':
+        start()
+    if query[0] == 'n':
+        print("closing application")
+        time.sleep(1.5)
+
+
+start()
